ed11ffe03d0da9709c05346914f892fbe96be164
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-09-19 18:36:51 +0800

    sentinel linear search

--- ts/search.js
+++ ts/search.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var hashMap = {};
 hashMap['apple'] = 5;
 hashMap['banana'] = 8;
@@ -8,5 +17,24 @@ var userMap = new Map();
 userMap.set(1, "Alice");
 userMap.set(2, "Bob");
 userMap.set(3, "Charlie");
-console.log(userMap.get(3));
-console.log(userMap.has(5));
+// console.log(userMap.get(3));
+// console.log(userMap.has(5));
+function sentinelLinearSearch(arr, target) {
+    var arrCopy = __spreadArray([], arr, true);
+    var n = arrCopy.length;
+    var lastElement = arrCopy[n - 1];
+    var i = 0;
+    while (arrCopy[i] !== target) {
+        i++;
+    }
+    arrCopy[n - 1] = lastElement;
+    if (i < n - 1 || arrCopy[n - 1] === target) {
+        return i;
+    }
+    else {
+        return -1;
+    }
+}
+var arr = [2, 4, 55, 11, 16, 13, 9, 90, 10, 12, 14];
+var result = sentinelLinearSearch(arr, 9);
+console.log("the target is on the ".concat(result + 1));
--- ts/search.ts
+++ ts/search.ts
@@ -15,5 +15,29 @@ userMap.set(2,"Bob");
 userMap.set(3, "Charlie");
 
 
-console.log(userMap.get(3));
-console.log(userMap.has(5));
+// console.log(userMap.get(3));
+// console.log(userMap.has(5));
+
+function sentinelLinearSearch(arr: number[],target:number){
+    const arrCopy = [...arr];
+    const n = arrCopy.length;
+    const lastElement = arrCopy[n-1];
+
+    let i = 0;
+
+    while(arrCopy[i] !== target){
+        i++;
+    }
+
+    arrCopy[n-1] = lastElement;
+
+    if(i<n-1 || arrCopy[n-1] === target){
+        return i;
+    }else{
+        return -1;
+    }
+}
+let arr = [2,4,55,11,16,13,9,90, 10, 12,14]
+let result = sentinelLinearSearch(arr, 9);
+
+console.log(`the target is on the ${result+1}`);
